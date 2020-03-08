728684459557c7b13629f16abf5aa2b2f12fc41f
Pj Abing <pj.abing@gmail.com>
2020-03-08 19:37:46 +0800

    Update for SMS feature

--- app/Http/Controllers/zEmailController.php
+++ app/Http/Controllers/zEmailController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use PharIo\Manifest\Email;
+use Nexmo\Laravel\Facade\Nexmo;
+
 
 class zEmailController extends Controller
 {
@@ -16,10 +18,23 @@ class zEmailController extends Controller
     {
 
         
+        // $contact = '09068002030';
+        // $newstring = substr($contact, -10);
+        // return '63'.$newstring;
+        // if (
+        //     $contact == '639507599270' ||
+        //     $contact == '639068002030' ||
+        //     $contact == '639564510415' ||
+        //     $contact == '639309008864'
+        // ) {
+        //     return $contact;
+        // } else {
+        //     return 'shit';
+        // }
 
 
-        $digits = 5;
-        $confirmation_code = rand(pow(10, $digits - 1), pow(10, $digits) - 1);
+         $digits = 5;
+         $confirmation_code = rand(pow(10, $digits - 1), pow(10, $digits) - 1);
         $beautymail = app()->make(\Snowfire\Beautymail\Beautymail::class);
         $beautymail->send(
             'emails.welcome',
@@ -27,13 +42,24 @@ class zEmailController extends Controller
             function ($message) {
                 $message
                     ->from('epawn.online01@gmail.com', 'E-PAWN')
-                    ->to('pj.abing@gmail.com', 'PJ Abing' )
+                    ->to('pj.abing@gmail.com', 'PJ Abing')
                     ->subject('E-pawn Email Verification!');
             }
         );
 
-
-
+        ///Send SMS 
+        // 639507599270
+        // 639564510415
+        // 639309008864
+        $contact = '639068002030';
+        $basic  = new \Nexmo\Client\Credentials\Basic('7d5f097e', 'BA5EPguxLE0jbEed');
+        $client = new \Nexmo\Client($basic);
+        $message = "Hi Pj Abing, your registration code is: [ " . $confirmation_code . " ]";
+        $client->message()->send([
+            'to' => $contact,
+            'from' => 'E-pawn',
+            'text' => $message
+        ]);
 
         return "Email Sent";
     }
